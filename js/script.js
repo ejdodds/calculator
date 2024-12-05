6e129dc6cd3202d86d84987e660b00c06e7ffe29
ejdodds <ejdodds25@gmail.com>
2024-12-05 14:46:16 +0800

    Add logic for the basic math operations
    I added the basic math operation functions:
    	- addition
    	- subtraction
    	- multiplication
    	- division

---
+++ js/script.js
@@ -0,0 +1,20 @@
+/* This function does the addition operation. */
+function add(addendA, addendB) {
+  return addendA + addendB;
+}
+
+/* This function does the subtraction operation. */
+function subtract(minuend, subtrahend) {
+  return minuend - subtrahend;
+}
+
+/* This function does the division operation. */
+function divide(dividend, divisor) {
+  return dividend / divisor;
+}
+
+/* This function does the multiplication operation. */
+function multiply(factorA, factorB) {
+  return factorA * factorB;
+}
+
